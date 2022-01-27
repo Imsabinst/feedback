b2aedc821b51c2c607cef569fcfb900bef84f256
sabin <sashre@utu.fi>
2022-01-27 18:31:30 +0200

    1.7 Feedback app, part 2

--- src/App.js
+++ src/App.js
@@ -5,33 +5,71 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      counter1: 0,
-      counter2: 0,
-      counter3: 0,
-      title: "Give Feedback",
-      result: "Statistics",
-      good: "good",
-      neutral: "neutral",
-      poor: "poor",
+      counterForGood: 0,
+      counterForNeutral: 0,
+      counterForPoor: 0,
+      countAvg: 0,
+      countPos: 0,
     };
   }
 
+  countAverage(good, neutral, poor) {
+    let sum = good + neutral + poor;
+    let finalData = isNaN((good - poor) / sum) ? 0 : (good - poor) / sum;
+    return finalData;
+  }
+  countPositive(good, neutral, poor) {
+    let sum = good + neutral + poor;
+    return (good * 100) / sum;
+  }
+
   good() {
-    this.setState({ counter1: this.state.counter1 + 1 });
+    this.setState({
+      counterForGood: this.state.counterForGood + 1,
+      countAvg: this.countAverage(
+        this.state.counterForGood,
+        this.state.counterForNeutral,
+        this.state.counterForPoor
+      ),
+    });
   }
 
   neutral() {
-    this.setState({ counter2: this.state.counter2 + 1 });
+    this.setState({
+      counterForNeutral: this.state.counterForNeutral + 1,
+      countAvg: this.countAverage(
+        this.state.counterForGood,
+        this.state.counterForNeutral,
+        this.state.counterForPoor
+      ),
+    });
   }
   poor() {
-    this.setState({ counter3: this.state.counter3 + 1 });
+    this.setState({
+      counterForPoor: this.state.counterForPoor + 1,
+      countAvg: this.countAverage(
+        this.state.counterForGood,
+        this.state.counterForNeutral,
+        this.state.counterForPoor
+      ),
+    });
+  }
+  statistics(good, neutral, poor) {
+    let denominator = good + neutral + poor;
+    if (denominator === 0) {
+      return { countAvg: 0, countPos: 0 };
+    } else
+      return {
+        countAvg: (good - poor) / denominator,
+        countPos: (good * 100) / denominator,
+      };
   }
 
   render() {
     return (
       <div>
         <div>
-          <h2>{this.state.title}</h2>
+          <h2>Give Feedback</h2>
         </div>
         <div>
           <button onClick={this.good.bind(this)}>good</button>
@@ -39,17 +77,13 @@ class App extends React.Component {
           <button onClick={this.poor.bind(this)}>poor</button>
         </div>
         <div>
-          <h2>{this.state.result}</h2>
+          <h2>Statistics</h2>
         </div>
-        <div>
-          {this.state.good}: {this.state.counter1}
-        </div>
-        <div>
-          {this.state.neutral}: {this.state.counter2}
-        </div>
-        <div>
-          {this.state.poor}: {this.state.counter3}
-        </div>
+        <div>Good: {this.state.counterForGood}</div>
+        <div>Neutral: {this.state.counterForNeutral}</div>
+        <div>Poor: {this.state.counterForPoor}</div>
+        <div>Average: {this.state.countAvg} </div>
+        <div>Positive: {this.state.countPos}%</div>
       </div>
     );
   }
